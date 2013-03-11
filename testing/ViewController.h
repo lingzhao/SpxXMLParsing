85e871da5f37aa6b35649b9854335556fbf7b200
John <john@Johns-MacBook-Pro.local>
2013-03-11 10:38:54 +0800

    Initial Commit

---
+++ testing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testing
+//
+//  Created by John on 3/11/13.
+//  Copyright (c) 2013 ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testing
+//
+//  Created by John on 3/11/13.
+//  Copyright (c) 2013 ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
